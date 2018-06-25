73849767939beb995769837c9752d6ef35901eef
Emmanuel <ogoma.emmanuel@gmail.com>
2018-06-25 11:18:15 +0300

    first commit

---
+++ src/posts/posts.js
@@ -0,0 +1,1 @@
+console.log("Emmanuel needs some water")
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    entry: ["./src/comments/comments.js","./src/posts/posts.js", "./app.js"],
+    output: {
+      filename: "main.js"
+    }
+  }
